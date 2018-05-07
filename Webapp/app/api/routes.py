06615291a20256a569db97ed68eef9918587326a
sleter <szy.janowski@gmail.com>
2018-05-07 12:32:41 +0200

    email working

--- Webapp/app/api/routes.py
+++ Webapp/app/api/routes.py
@@ -8,5 +8,7 @@ def do():
     """Processing JSON file. To check how it works: curl -X POST http://127.0.0.1:5000/do --data @nazwa_jsona.json"""
     data = request.get_json(force=True)
     js = json.dumps(data)
+    #print(js)
     new_js = change.respond(js)
+    #print(new_js)
     return jsonify(new_js)
--- Webapp/app/config.py
+++ Webapp/app/config.py
@@ -4,9 +4,10 @@ class Config:
     # needed for RegistrationForm and LoginForm -> protect against modyfing cookies, cross site request forgery attack
     SECRET_KEY = '55d102a68a11bdb91578610ca74640a3'
     SQLALCHEMY_DATABASE_URI = 'postgres://ddlotpgl:62z66ApgTiJXDhajQl7GhyAgaku933yo@horton.elephantsql.com:5432/ddlotpgl'
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     # REMEMBER TO CHANGE
-    MAIL_USERNAME = 'username'
-    MAIL_PASSWORD = 'password'
+    MAIL_USERNAME = 'brovie.filmchatbot@gmail.com'
+    MAIL_PASSWORD = 'botpassword'
