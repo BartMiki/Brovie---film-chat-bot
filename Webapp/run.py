d73ff9b2cb3c3394d1725b5742196ac6d2fc88c0
Agulaa <agnieszkaxrusin@gmail.com>
2018-05-15 07:57:58 +0200

    update bot

--- Webapp/run.py
+++ Webapp/run.py
@@ -2,5 +2,11 @@ from app import create_app
 
 app = create_app()
 
+
+@app.route('/')
+def index():
+    return app
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
